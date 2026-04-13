b4d8efa4baab7f11a250f3d369afb5d283aadebc
521w <54992297+521w@users.noreply.github.com>
2026-04-13 09:48:01 +0000

    更新 NetMonitorApp.kt

--- app/src/main/java/com/netmonitor/app/NetMonitorApp.kt
+++ app/src/main/java/com/netmonitor/app/NetMonitorApp.kt
@@ -3,7 +3,6 @@ package com.netmonitor.app
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.os.Build
 
 class NetMonitorApp : Application() {
 
